fde5c9c909884842a23a350008cce64024041a4f
Raphaël Chalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-03-07 21:26:57 +0100

    Backtick au lieu d'apostrophe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,7 +23,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add('HeroService: Selected hero id=${hero.id}')
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
